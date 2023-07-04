b2956e198ebc65a2218143221064e8721edd8c68
Vaidas D <vaidasdbr@gmail.com>
2023-07-04 00:44:21 -0700

    projektas12

---
+++ r1/src/Components/004/AnimalItem.jsx
@@ -0,0 +1,21 @@
+import "../../style.scss";
+import rand from "../../Functions/rand.js";
+import randColor from "../../Functions/randColor.js";
+function AnimalItem({ animal }) {
+    let handleClick = parameter => {
+        console.log(parameter, rand(1, 100));
+        console.log(animal.type);
+    }
+    return (
+        <>
+            <div className="box" style={{
+                color: animal.animalColor,
+                backgroundColor: randColor(),
+                }}>
+                <h2>{animal.type}<span>***</span></h2>
+            </div>
+            <button onClick={_ => handleClick(222)}>Click Me</button>
+        </>
+    );
+}
+export default AnimalItem;
